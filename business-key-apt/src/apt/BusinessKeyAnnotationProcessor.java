d7d4257c5b65c4ab188d3a7a124f2d1308860349
abhishekbiyala <abhishekbiyala@gmail.com>
2013-03-30 22:08:41 +0530

    Using new Java 1.6 based Processor

--- business-key-apt/src/apt/BusinessKeyAnnotationProcessor.java
+++ business-key-apt/src/apt/BusinessKeyAnnotationProcessor.java
@@ -1,67 +1,23 @@
 package apt;
 
-import java.util.Collection;
-import java.util.Map;
+import java.util.Set;
 
-import com.sun.mirror.apt.AnnotationProcessor;
-import com.sun.mirror.apt.AnnotationProcessorEnvironment;
-import com.sun.mirror.declaration.AnnotationMirror;
-import com.sun.mirror.declaration.AnnotationTypeDeclaration;
-import com.sun.mirror.declaration.AnnotationTypeElementDeclaration;
-import com.sun.mirror.declaration.AnnotationValue;
-import com.sun.mirror.declaration.Declaration;
-import com.sun.mirror.util.SourcePosition;
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
 
-public class BusinessKeyAnnotationProcessor implements AnnotationProcessor {
-
-	private AnnotationProcessorEnvironment environment;
-
-	private AnnotationTypeDeclaration businessKeyDeclaration;
-	
-	public BusinessKeyAnnotationProcessor() {
-		super();
-	}
-
-	public BusinessKeyAnnotationProcessor(
-			AnnotationProcessorEnvironment environment) {
-		super();
-		this.environment = environment;
-
-		businessKeyDeclaration = (AnnotationTypeDeclaration) environment
-				.getTypeDeclaration("annotation.BusinessKey");
-	}
+@SupportedAnnotationTypes("annotation.BusinessKey")
+public class BusinessKeyAnnotationProcessor extends AbstractProcessor {
 
 	@Override
-	public void process() {
-		Collection<Declaration> declarations = environment
-				.getDeclarationsAnnotatedWith(businessKeyDeclaration);
-		for (Declaration declaration : declarations) {
-			processNoteAnnotations(declaration);
+	public boolean process(Set<? extends TypeElement> annotations,
+			RoundEnvironment roundEnv) {
+		for (Element e : roundEnv.getRootElements()) {
+			System.out.println("Element is " + e.getSimpleName());
 		}
-	}
-
-	private void processNoteAnnotations(Declaration declaration) {
-		Collection<AnnotationMirror> annotations = declaration
-				.getAnnotationMirrors();
-		for (AnnotationMirror mirror : annotations) {
-			if (mirror.getAnnotationType().getDeclaration()
-					.equals(businessKeyDeclaration)) {
-
-				SourcePosition position = mirror.getPosition();
-				Map<AnnotationTypeElementDeclaration, AnnotationValue> values = mirror
-						.getElementValues();
-
-				System.out.println("Declaration: " + declaration.toString());
-				System.out.println("Position: " + position);
-				System.out.println("Values:");
-				for (Map.Entry<AnnotationTypeElementDeclaration, AnnotationValue> entry : values
-						.entrySet()) {
-					AnnotationTypeElementDeclaration elemDecl = entry.getKey();
-					AnnotationValue value = entry.getValue();
-					System.out.println("    " + elemDecl + "=" + value);
-				}
-			}
-		}
+		return true;
 	}
 
 }
